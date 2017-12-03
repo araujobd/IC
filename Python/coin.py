7535c5d268c96220bde266002b6cf534d92ac5b5
Bruno Dantas <bdantas47@hotmail.com>
2017-12-02 21:04:53 -0300

    coin

--- Python/coin.py
+++ Python/coin.py
@@ -1,33 +1,50 @@
 import random
 
-def save_values(value):
-    save = open('values.csv', 'w')
+def save_values(value, values = 'values.csv'):
+    save = open(values, 'w')
     content = []
     for k, v in value.items():
-        content.append(str(k) + ',' + str(v) + '\n')
+        content.append(str(k) + ' ' + str(v) + '\n')
     save.writelines(content)
     save.close()
 
-def read_values():
+def read_values(values = 'values.csv'):
     try:
-        save = open('values.csv', 'r')
+        save = open(values, 'r')
         lines = save.readlines()
-        print(...)
+
+        value = {}
+        for i in lines:
+            k, v = i.strip().split(' ')
+            value[int(k)] = float(v)
+        #print(...)
     except:
         return initialize()
-        
 
 def rand_play(state, p):
     a = random.randint(1, min(state, 100 - state))
-    if (random.random() <= p):
+    if (random.random() >= p):
         s = state + a
     else:
         s = state - a
     return s
 
+def best_play(value, state):
+    best = 1
+    for i in range(1, min(state, (100 - state))):
+        if (value[i] >= value[best]):
+            best = i
+    return best
+
+def generate_graph(value):
+    aux = {}
+    for i in range(1, 100):
+        aux[i] = best_play(value, i)
+    save_values(aux, 'graph.csv')
+
 def initialize():
     v = {}
-    for i in range(0,100):
+    for i in range(0, 100):
         v[i] = 0
     v[100] = 1
     return v
@@ -39,15 +56,14 @@ def main():
     count = 0
     try:
         while(True):
-            for state in range(1,100):
+            for state in range(1, 100):
                 next_state = rand_play(state, p)
                 aux = value[next_state] - value[state]
                 value[state] = value[state] + (alfa * aux)
-            count = count + 1
+            count += 1
     except:
         save_values(value)
-        print(value.items())
-        print()
+        generate_graph(value)
         print(count)
 
 main()
